0631e31edaa8a8d1d17a7f333b3ca5cfdc251a48
Ludmal <ludmal@gmail.com>
2023-12-30 08:12:21 +1100

    initial commit

--- src/Infonex.AI/InfonexAI/Endpoints/v1/Workspace.cs
+++
@@ -1,7 +0,0 @@
-namespace WebApplication2.Endpoints.v1;
-
-public static class Workspace
-{
-    public static async Task<IResult> Create(HttpContext ctx, HttpClient httpClient)
-        => await Task.FromResult(Results.Ok("created"));
-}
